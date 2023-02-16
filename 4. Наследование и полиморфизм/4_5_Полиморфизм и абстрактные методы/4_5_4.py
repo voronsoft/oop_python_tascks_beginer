35e0253368aaf6a02e6c58f98231735e5282cc21
voronsoft <gbo4.net@gmail.com>
2023-02-16 22:02:07 +0200

    Добавление директории : 4. Наследование и полиморфизм

---
+++ 4. Наследование и полиморфизм/4_5_Полиморфизм и абстрактные методы/4_5_4.py
@@ -0,0 +1,53 @@
+"""
+Подвиг 4. Вам необходимо объявить базовый класс ShopInterface с абстрактным методом:
+
+def get_id(self): ...
+В самом методе должно генерироваться исключение командой:
+raise NotImplementedError('в классе не переопределен метод get_id')
+Инициализатор в классе ShopInterface прописывать не нужно.
+
+Далее объявите дочерний класс ShopItem (от базового класса ShopInterface), объекты которого создаются командой:
+
+item = ShopItem(name, weight, price)
+где name - название товара (строка);
+weight - вес товара (любое положительное число);
+price - цена товара (любое положительное число).
+
+В каждом объекте класса ShopItem должны формироваться локальные атрибуты с именами _name, _weight, _price и соответствующими значениями.
+Также в объектах класса ShopItem должен автоматически формироваться локальный приватный атрибут __id с уникальным (для каждого товара) целым значением.
+
+В классе ShopItem необходимо переопределить метод get_id() базового класса так, чтобы он (метод) возвращал значение атрибута __id.
+
+P.S. В программе требуется объявить только классы. На экран выводить ничего не нужно.
+"""
+
+
+class ShopInterface:
+    def get_id(self):
+        raise NotImplementedError('в классе не переопределен метод get_id')
+
+
+class ShopItem(ShopInterface):
+    ID = 0
+
+    def __init__(self, name, weight, price):
+        self._name = str(name)
+        self._weight = weight if weight >= 0 and type(weight) in (int, float) else None
+        self._price = price if price >= 0 and type(price) in (int, float) else None
+        self.__id = self.gen_id()
+
+    @classmethod
+    def gen_id(cls):
+        cls.ID += 1
+        return cls.ID
+
+    def get_id(self):
+        return self.__id
+
+
+# # TEST
+# sh1 = ShopItem('имя', 0.10, 100)
+# id_sh1 = sh1.get_id()
+#
+# sh2 = ShopItem('имя', 0.10, 100)
+# id_sh2 = sh2.get_id()
---
+++ 4. Наследование и полиморфизм/4_6_Множественное наследование ООП/4_6_9.py
@@ -0,0 +1,122 @@
+"""
+Подвиг 9. Объявите класс с именем Money (деньги), объекты которого создаются командой:
+
+money = Money(value)
+где value - любое число (целое или вещественное). Если указывается не числовое значение, то генерируется исключение командой:
+
+raise TypeError('сумма должна быть числом')
+В каждом объекте этого класса должен формироваться локальный атрибут _money с соответствующим значением.
+Также в классе Money должно быть объект-свойство (property):
+money - для записи и считывания значения из атрибута _money.
+
+В связке с классом Money работает еще один класс:
+
+class MoneyOperators:
+    def __add__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money + other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money + other.money)
+Он определяет работу арифметических операторов.
+В данном примере описан алгоритм сложения двух объектов класса Money (или объектов его дочерних классов).
+
+Обратите внимание, как реализован метод __add__() в этом классе.
+Он универсален при работе с любыми объектами класса Money или его дочерних классов.
+Здесь атрибут __class__ - это ссылка на класс объекта self. С помощью __class__ можно создавать объекты того же класса, что и self.
+
+Вам необходимо добавить в класс MoneyOperators аналогичную реализацию оператора вычитания.
+
+На основе двух классов (Money и MoneyOperators) предполагается создавать классы кошельков разных валют. Например, так:
+
+class MoneyR(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyR: {self.money}"
+
+
+class MoneyD(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyD: {self.money}"
+И, затем применять их следующим образом (эти строчки в программе писать не нужно):
+
+m1 = MoneyR(1)
+m2 = MoneyD(2)
+m = m1 + 10
+print(m)  # MoneyR: 11
+m = m1 - 5.4
+m = m1 + m2  # TypeError
+P.S. В программе требуется объявить только классы. На экран выводить ничего не нужно.
+"""
+
+
+class MoneyOperators:
+    def __add__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money + other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money + other.money)
+
+    def __radd__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(other + self.money)
+
+    # здесь объявляйте еще один метод
+    # Вам необходимо добавить в класс MoneyOperators аналогичную реализацию оператора вычитания.
+    # __sub__(self, other) - вычитание (x - y).
+    # Пример m = m1 - 5.4
+    def __sub__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money - other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money - other.money)
+
+    def __rsub__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(other - self.money)
+
+
+# здесь объявляйте класс Money
+class Money:
+    def __init__(self, value):
+        if type(value) in (int, float):
+            self._money = value
+        else:
+            raise TypeError('сумма должна быть числом')
+
+    # Также в классе Money должно быть объект-свойство (property):
+    # money - для записи и считывания значения из атрибута _money.
+    @property
+    def money(self):
+        return self._money
+
+    @money.setter
+    def money(self, value):
+        self._money = value
+
+
+class MoneyR(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyR: {self.money}"
+
+
+class MoneyD(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyD: {self.money}"
+
+# TEST
+# m1 = MoneyR(100)
+# m2 = MoneyR(2)
+# m = m1 + 10
+# print(m)  # MoneyR: 11
+# m = m1 - 5.4
+# m = m1 - m2  # TypeError
+# m = 10 - m1  # TypeError
+# P.S. В программе требуется объявить только классы. На экран выводить ничего не нужно.
