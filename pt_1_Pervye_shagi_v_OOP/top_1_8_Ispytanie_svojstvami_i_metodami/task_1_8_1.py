a0d93e66ba52a0ac2443b7df1f1b53eefef9bed3
voronsoft <gbo4.net@gmail.com>
2023-03-31 22:02:50 +0300

    Перенос тестов из файлов с заданиями в папку для тестирования. top_1_8_Ispytanie_svojstvami_i_metodami

--- pt_1_Pervye_shagi_v_OOP/top_1_8_Ispytanie_svojstvami_i_metodami/task_1_8_1.py
+++ pt_1_Pervye_shagi_v_OOP/top_1_8_Ispytanie_svojstvami_i_metodami/task_1_8_1.py
@@ -53,6 +53,7 @@ ip - IP-адрес назначения.
 """
 
 
+# ваш код:
 # -Router-
 # Для формирования и функционирования локальной сети, в классе Router должны быть реализованы следующие методы:
 # -link(server) - для присоединения сервера server (объекта класса Server) к роутеру
@@ -66,7 +67,9 @@ ip - IP-адрес назначения.
 class Router:
     # И одно обязательное локальное свойство (могут быть и другие свойства):
     # -buffer - список для хранения принятых от серверов пакетов (объектов класса Data).
-    def __init__(self, connect={}):
+    def __init__(self, connect=None):
+        if connect is None:
+            connect = {}
         self.buffer = list()
         self.connect_router_server = connect
 
@@ -115,9 +118,8 @@ class Server:
     # (пакет отправляется роутеру и сохраняется в его буфере - локальном свойстве buffer);
     # sv_from.send_data(Data("Hello", sv_to.get_ip()))
     def send_data(self, data):
-        if (self.connect_router_server != None) and (self.ip in router.connect_router_server):
-            # router.buffer.append((data.ip, data.data))
-            router.buffer.append(data)
+        if self.connect_router_server is not None:
+            self.connect_router_server.buffer.append(data)
 
     # get_data() - возвращает список принятых пакетов (если ничего принято не было, то возвращается пустой список)
     # и очищает входной буфер;
@@ -141,67 +143,8 @@ class Data:
         self.ip = ip
 
 
-# Пример использования этих классов (эти строчки в программе писать не нужно):
-router = Router()
-sv_from = Server()
-sv_from2 = Server()
-router.link(sv_from)
-router.link(sv_from2)
-# # router.unlink(sv_from)
-# # router.unlink(sv_from2)
-router.link(sv_from2)
-router.link(Server())
-router.link(Server())
-sv_to = Server()
-router.link(sv_to)
-#
-sv_from.send_data(Data("Hello", sv_to.get_ip()))
-sv_from2.send_data(Data("Hello", sv_to.get_ip()))
-sv_to.send_data(Data("Hi", sv_from.get_ip()))
-#
-router.send_data()
-msg_lst_from = sv_from.get_data()
-msg_lst_to = sv_to.get_data()
-# Ваша задача реализовать классы Router, Server и Data в соответствии с приведенным техническим заданием (ТЗ).
-# Что-либо выводить на экран не нужно.
-
-
 # TEST-TASK___________________________________
-assert hasattr(Router, 'link') and hasattr(Router, 'unlink') and hasattr(Router,
-                                                                         'send_data'), "в классе Router присутсвутю не все методы, указанные в задании"
-assert hasattr(Server, 'send_data') and hasattr(Server, 'get_data') and hasattr(Server,
-                                                                                'get_ip'), "в классе Server присутсвутю не все методы, указанные в задании"
+from test1_8.test_1_8_1 import test_1
 
-router = Router()
-sv_from = Server()
-sv_from2 = Server()
-router.link(sv_from)
-router.link(sv_from2)
-router.link(Server())
-router.link(Server())
-sv_to = Server()
-router.link(sv_to)
-sv_from.send_data(Data("Hello", sv_to.get_ip()))
-sv_from2.send_data(Data("Hello", sv_to.get_ip()))
-sv_to.send_data(Data("Hi", sv_from.get_ip()))
-router.send_data()
-msg_lst_from = sv_from.get_data()
-msg_lst_to = sv_to.get_data()
-
-assert len(router.buffer) == 0, "после отправки сообщений буфер в роутере должен очищаться"
-assert len(sv_from.buffer) == 0, "после получения сообщений буфер сервера должен очищаться"
-
-assert len(msg_lst_to) == 2, "метод get_data вернул неверное число пакетов"
-
-assert msg_lst_from[0].data == "Hi" and msg_lst_to[
-    0].data == "Hello", "данные не прошли по сети, классы не функционируют должным образом"
-
-assert hasattr(router, 'buffer') and hasattr(sv_to,
-                                             'buffer'), "в объектах классов Router и/или Server отсутствует локальный атрибут buffer"
-
-router.unlink(sv_to)
-sv_from.send_data(Data("Hello", sv_to.get_ip()))
-router.send_data()
-msg_lst_to = sv_to.get_data()
-assert msg_lst_to == [], "метод get_data() вернул неверные данные, возможно, неправильно работает метод unlink()"
-print("Так держать, правильный ответ !")
+test_1(Router, Server, Data)
+# END
